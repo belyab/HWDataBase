83325859eeaa25b4e95553d48817d36d29b36510
belyab <72295958+belyab@users.noreply.github.com>
2021-10-05 23:10:23 +0300

    update

--- HW2/V7__Create_client_table.sql
+++ HW2/V7__Create_client_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE client(
                        client_id serial PRIMARY KEY,
                        full_name VARCHAR(255) NOT NULL,
-                       passport INT NOT NULL,
-                       phone_number INT NOT NULL
+                       passport BIGINT NOT NULL,
+                       phone_number BIGINT NOT NULL
 )
--- HW2/V8__Create_employee_table.sql
+++ HW2/V8__Create_employee_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE employee(
                          employee_id serial PRIMARY KEY,
                          full_name VARCHAR(255) NOT NULL,
-                         phone_number INT NOT NULL
+                         phone_number BIGINT NOT NULL
 )
