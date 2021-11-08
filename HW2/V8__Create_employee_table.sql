90448a5c718db9dac44e1f996a7a138483c5d1e9
belyab <72295958+belyab@users.noreply.github.com>
2021-11-08 20:52:06 +0300

    Update V8__Create_employee_table.sql

--- HW2/V8__Create_employee_table.sql
+++ HW2/V8__Create_employee_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE employee(
                          employee_id serial PRIMARY KEY,
-                         full_name VARCHAR(255) NOT NULL,
-                         phone_number BIGINT NOT NULL
+                         full_name VARCHAR(255) NOT NULL check(char_length(full_name) > 0 ),
+                         phone_number varchar(11) NOT NULL check (char_length(phone_number) = 11 )
 )
