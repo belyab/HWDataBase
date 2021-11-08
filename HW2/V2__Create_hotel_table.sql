8201e2180436f078b7b66be0416ec9e4b20ce211
belyab <72295958+belyab@users.noreply.github.com>
2021-11-08 20:40:44 +0300

    Update V2__Create_hotel_table.sql

--- HW2/V2__Create_hotel_table.sql
+++ HW2/V2__Create_hotel_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE hotel(
                       hotel_id serial PRIMARY KEY,
                       country_id INT ,
-                      category INT NOT NULL,
+                      category INT NOT NULL CHECK (category >= 0),
                       country_name VARCHAR(255) NOT NULL,
                       CONSTRAINT country_id FOREIGN KEY (country_id) REFERENCES country(country_id)
 )
