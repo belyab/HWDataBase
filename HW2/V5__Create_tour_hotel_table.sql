a0256813de2e2e59b63f54c0f15ee7876240e6cd
belyab <72295958+belyab@users.noreply.github.com>
2021-11-08 20:47:52 +0300

    Create V5__Create_tour_hotel_table.sql

--- HW2/V5__Create_tour_hotel_table.sql
+++ HW2/V5__Create_tour_hotel_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE tour_hotel(
-                           hotel_id INT not null ,
-                           tour_id INT not null,
+                           hotel_id INT,
+                           tour_id INT,
                            CONSTRAINT hotel_id FOREIGN KEY (hotel_id) REFERENCES hotel(hotel_id),
                            CONSTRAINT tour_id FOREIGN KEY (tour_id) REFERENCES tour(tour_id)
 )
