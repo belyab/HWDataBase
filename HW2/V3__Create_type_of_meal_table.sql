ec0dca1d21bbf9946e90c1c113a0252059c41349
belyab <72295958+belyab@users.noreply.github.com>
2021-11-08 20:42:59 +0300

    Update V3__Create_type_of_meal_table.sql

--- HW2/V3__Create_type_of_meal_table.sql
+++ HW2/V3__Create_type_of_meal_table.sql
@@ -1,4 +1,4 @@
 CREATE TABLE type_of_meal(
                              type_of_meal_id serial PRIMARY KEY,
-                             type_of_meal VARCHAR(10) NOT NULL
+                             type_of_meal VARCHAR(10) NOT NULL CHECK (type_of_mael = "RO" || type_of_mael = "BB" || type_of_mael = "HB" || type_of_mael = "AI" || type_of_mael = "UAI")
 )
