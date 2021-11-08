08fb2cc45214ef772837e0b020a6638562aafab1
belyab <72295958+belyab@users.noreply.github.com>
2021-11-08 20:50:54 +0300

    Update V7__Create_client_table.sql

--- HW2/V7__Create_client_table.sql
+++ HW2/V7__Create_client_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE client(
                        client_id serial PRIMARY KEY,
-                       full_name VARCHAR(255) NOT NULL,
-                       passport BIGINT NOT NULL,
-                       phone_number BIGINT NOT NULL
+                       full_name VARCHAR(255) NOT NULL CHECK (char_length(full_name) > 1),
+                       passport varchar(10) NOT NULL (char_length(passport) =10 ) ,
+                       phone_number varchar(11) NOT NULL (char_length(passport) =11 )
 )
