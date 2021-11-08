c0dbd772ead52e7080e98445718b023aace4dd49
belyab <72295958+belyab@users.noreply.github.com>
2021-11-08 20:52:46 +0300

    Update V9__Create_sale_table.sql

--- HW2/V9__Create_sale_table.sql
+++ HW2/V9__Create_sale_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE sale(
                      employee_id INT NOT NULL,
                      tour_id INT NOT NULL,
                      date CHAR(11) NOT NULL,
-                     count INT NOT NULL,
+                     count INT NOT NULL check (count > 0),
                      CONSTRAINT client_id FOREIGN KEY (client_id) REFERENCES client (client_id),
                      CONSTRAINT employee_id FOREIGN KEY (employee_id) REFERENCES employee (employee_id),
                      CONSTRAINT tour_id FOREIGN KEY (tour_id) REFERENCES tour (tour_id)
